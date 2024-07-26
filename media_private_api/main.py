56eea1a56fe071e414b55c4162450fa22af96e25
smintank <assbestmail@gmail.com>
2024-07-26 16:24:18 +0400

    Add MinIO integration to media_private_api
    Introduced MinIO client configurations and bucket setup in media_private_api/main.py. Updated docker-compose.yml to use a shared network for all services. Refined Dockerfile and .env handling for better dependency management.

--- media_private_api/main.py
+++ media_private_api/main.py
@@ -1,13 +1,40 @@
+import os
+
 from fastapi import FastAPI, UploadFile, File
+from minio import S3Error, Minio
 
-app = FastAPI(title="S3_private_service")
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MINIO_URL = os.getenv("MINIO_URL")
+MINIO_ROOT_USER = os.getenv("MINIO_ROOT_USER")
+MINIO_ROOT_PASSWORD = os.getenv("MINIO_ROOT_PASSWORD")
+MINIO_BUCKET_NAME = os.getenv("MINIO_BUCKET_NAME")
 
 
-@app.get("/download")
-async def get_file():
-    pass
+minio_client = Minio(
+    MINIO_URL,
+    access_key=MINIO_ROOT_USER,
+    secret_key=MINIO_ROOT_PASSWORD,
+    secure=False
+)
+
+if not minio_client.bucket_exists(MINIO_BUCKET_NAME):
+    minio_client.make_bucket(MINIO_BUCKET_NAME)
+
+app = FastAPI(title="Private Media API")
+
+
+@app.get("/download/{file_id}")
+async def get_file(file_id: str):
+    try:
+        response = minio_client.get_object(MINIO_BUCKET_NAME, file_id)
+        return {"id": file_id, "content": response.data.decode('utf-8')}
+    except S3Error as e:
+        return {"error": str(e)}
 
 
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
-    pass
+    return await file.read()
